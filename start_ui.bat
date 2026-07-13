0d50008b878864fdb91c73c2817ac4489dfa5d05
Tyler Nguyen <wgq4tr@virginia.edu>
2026-07-13 11:35:04 -0400

    add start_ui.bat

---
+++ start_ui.bat
@@ -0,0 +1,7 @@
+@echo off
+rem Launches the automated-shorts web console at http://127.0.0.1:8000
+rem Double-click this file, keep the window open while you use the UI.
+cd /d "%~dp0"
+start "" http://127.0.0.1:8000
+python -m uvicorn web.server:app --port 8000
+pause
